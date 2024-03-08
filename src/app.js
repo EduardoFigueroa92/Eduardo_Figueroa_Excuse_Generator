f3b922852eb3b3fa74793a281ffabe418b1d6a72
Eduardo Figueroa <156800901+EduardoFigueroa92@users.noreply.github.com>
2024-03-08 13:16:30 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 /* eslint-disable */
-import "bootstrap";
+//import "bootstrap";
 import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
@@ -9,3 +9,36 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+//* eslint-disable *
+//import "bootstrap";
+//import "./style.css";
+
+//import "./assets/img/rigo-baby.jpg";
+//import "./assets/img/4geeks.ico";
+
+let pronoun = ["A", "The"];
+let subject = ["jogger", "racoon", "dog", "driver", "comedian"];
+let action = ["took my", "threw my", "yelled at my", "stole my", "bit my"];
+let possetion = ["homework", "toe", "car", "shoe"];
+let where = ["on the street", "in my house", "in my driveway"];
+const getRandomElement = listElement => {
+  let element = "";
+  element = listElement[Math.floor(Math.random() * listElement.length)];
+  console.log(Element);
+  return element;
+};
+
+const onload = () => {
+  let excuseGen = document.querySelector("#excuse");
+  excuseGen.innerHTML =
+    getRandomElement(pronoun) +
+    " " +
+    getRandomElement(subject) +
+    " " +
+    getRandomElement(action) +
+    " " +
+    getRandomElement(possetion) +
+    " " +
+    getRandomElement(where);
+};
+window.onload = onload();
